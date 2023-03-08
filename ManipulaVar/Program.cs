938c636d6bc0da08c01228f3d07e6949243142e0
Maggiee <telesmaegarida@gmail.com>
2023-03-08 14:53:53 +0000

    Projeto ManipulaVar

---
+++ ManipulaVar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ManipulaVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
