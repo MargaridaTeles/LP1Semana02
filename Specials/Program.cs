b53d0319a107589e962ee4bbc6444051603b6476
Maggiee <telesmaegarida@gmail.com>
2023-03-09 23:02:47 +0000

    Máximos e Minímos

--- Specials/Program.cs
+++ Specials/Program.cs
@@ -6,7 +6,36 @@ namespace Specials
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            sbyte sby = sbyte.MaxValue;
+            sbyte sb = sbyte.MinValue;
+            Console.WriteLine($"---SBYTE---\nMáximo: {sby}\nMinímo: {sb}");
+
+            double doub = double.MaxValue;
+            double db = double.MinValue;
+            Console.WriteLine($"\n---DOUBLE---\nMáximo: {doub}\nMinímo: {db}");
+
+            ulong ulo = ulong.MaxValue;
+            ulong ul = ulong.MinValue;
+            Console.WriteLine($"\n---ULONG---\nMáximo: {ulo}\nMinímo: {ul}");
+
+            int inte = int.MaxValue;
+            int it = int.MinValue;
+            Console.WriteLine($"\n---INT---\nMáximo: {inte}\nMinímo: {it}");
+
+            float flo = float.MaxValue;
+            float fl = float.MinValue;
+            Console.WriteLine($"\n---FLOAT---\nMáximo: {flo}\nMinímo: {fl}");
+
+            short sho = short.MaxValue;
+            short sh = short.MinValue;            
+            Console.WriteLine($"\n---SHORT---\nMáximo: {sho}\nMinímo: {sh}");
+
+            decimal dec = decimal.MaxValue;
+            decimal de = decimal.MinValue;
+            Console.WriteLine($"\n---DECIMAL---\nMáximo: {dec}\nMinímo: {de}");
+
+
+
         }
     }
 }
