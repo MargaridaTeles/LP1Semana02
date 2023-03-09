c1a811971216c41c636dd258373e18dea8fa79d4
Maggiee <telesmaegarida@gmail.com>
2023-03-09 22:22:29 +0000

    Pedir medidas ao utilizador

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -4,9 +4,23 @@ namespace PerguntaValores
 {
     class Program
     {
+        /// <summary>
+        /// V = πr2a Sa = 2πr(r + a) π = 3.1415926
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            float π = 3.1415926f;
+
+            //Pedir medidas ao utilizador
+            Console.Write($"Insira um número para o raio: ");
+            int r = int.Parse(Console.ReadLine());
+            Console.Write($"Insira uma altura: ");
+            int a = int.Parse(Console.ReadLine());
+
+
+
         }
     }
 }
