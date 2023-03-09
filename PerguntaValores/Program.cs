3bbb5d3782291a5b514804151462595ba9bfc4f0
Maggiee <telesmaegarida@gmail.com>
2023-03-09 22:57:16 +0000

    Atualização dos textos

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -12,9 +12,9 @@ namespace PerguntaValores
         {
             float pi = Convert.ToSingle(Math.PI);
             //Pedir medidas ao utilizador
-            Console.Write($"Insira um número para o raio: ");
+            Console.Write("Insira um número para o raio: ");
             int r = int.Parse(Console.ReadLine());
-            Console.Write($"Insira uma altura: ");
+            Console.Write("Insira uma altura: ");
             int a = int.Parse(Console.ReadLine());
 
             float r_quadrado = Convert.ToSingle(Math.Pow(r, 2));
@@ -23,8 +23,8 @@ namespace PerguntaValores
             float Sa = 2 * pi * r * (r + a);
 
             //Mostra resultado ao utilizador
-            Console.WriteLine($"O volume do cilíndro é: " + V
-                        +"\nA área da superfície do cilindro é: " + Sa);
+            Console.WriteLine($"O volume do cilíndro é: {V}"+
+                                $"\nA área da superfície do cilindro é: {Sa}");
 
 
 
