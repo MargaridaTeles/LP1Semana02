a850efd9a6496a5a64a52d4f2baf115828454133
Maggiee <telesmaegarida@gmail.com>
2023-03-10 12:19:30 +0000

    Cria triangulo com o numero de linhas inserido e com o caracter

---
+++ Triangle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Recebe o a altura do triangulo e caracter
+            int linha = int.Parse(args[0]);
+            int alturatri = linha;
+            string carater = args[1];
+
+            for (int a = 0; a < alturatri; a+=1)
+            {
+                    for (int e = 0; e <= a; e+=1)
+                    {
+                        Console.Write($"{carater}");
+                    }
+                    Console.Write("\n");
+            }
+        }
+    }
+}
