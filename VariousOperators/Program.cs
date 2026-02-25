84291cb22d383fb9c713b4824da716b48f3cd8fe
MargaridaTeles <telesmaegarida@gmail.com>
2026-02-25 19:42:15 +0000

    Add byte operations: division by 2, left shift, XOR with 6, and comparison

--- VariousOperators/Program.cs
+++ VariousOperators/Program.cs
@@ -8,6 +8,14 @@ namespace VariousOperators
         {
             Console.Write("Insira número inteiro-não negativo: ");
             uint input = uint.Parse(Console.ReadLine());
+
+            byte n = Convert.ToByte(input);
+
+            Console.WriteLine($"Divisão por 2: {n/2}");
+            Console.WriteLine($"Shift Left por 3: {n << 3}");
+
+            Console.WriteLine($"XOR com 6: {n ^ 6}");
+            Console.WriteLine($"É maior que 10? {n > 10}");
         }
     }
 }
