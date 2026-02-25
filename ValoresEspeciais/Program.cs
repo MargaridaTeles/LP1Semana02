44c87b9f98aecdc166dfe5e40538e7897b5ba81f
MargaridaTeles <telesmaegarida@gmail.com>
2026-02-25 20:03:15 +0000

    Add print min and max values of all known types

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -6,7 +6,17 @@ namespace ValoresEspeciais
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            Console.WriteLine($"sbyte: Min = {sbyte.MinValue}, Max = {sbyte.MaxValue}");
+            Console.WriteLine($"byte: Min = {byte.MinValue}, Max = {byte.MaxValue}");
+            Console.WriteLine($"short: Min = {short.MinValue}, Max = {short.MaxValue}");
+            Console.WriteLine($"ushort: Min = {ushort.MinValue}, Max = {ushort.MaxValue}");
+            Console.WriteLine($"int: Min = {int.MinValue}, Max = {int.MaxValue}");
+            Console.WriteLine($"uint: Min = {uint.MinValue}, Max = {uint.MaxValue}");
+            Console.WriteLine($"long: Min = {long.MinValue}, Max = {long.MaxValue}");
+            Console.WriteLine($"ulong: Min = {ulong.MinValue}, Max = {ulong.MaxValue}");
+            Console.WriteLine($"float: Min = {float.MinValue}, Max = {float.MaxValue}");
+            Console.WriteLine($"double: Min = {double.MinValue}, Max = {double.MaxValue}");
+            Console.WriteLine($"decimal: Min = {decimal.MinValue}, Max = {decimal.MaxValue}");
         }
     }
 }
