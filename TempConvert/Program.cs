8b94b344f37d5de60de6887b37a37e0dd83ea8c9
MargaridaTeles <telesmaegarida@gmail.com>
2026-02-26 16:22:10 +0000

    Add mensages when finish the operations

--- TempConvert/Program.cs
+++ TempConvert/Program.cs
@@ -6,9 +6,9 @@ namespace TempConvert
     {
         private static void Main(string[] args)
         {
-            Console.Write("Insira um temperatura: ");
+            Console.Write("Temperature: ");
             string input = Console.ReadLine();
-            Console.Write("Insira a unidade de origem (C) Celsious ou (F) Fahrenheit: ");
+            Console.Write("Unit (C/F): ");
             string unidade = Console.ReadLine();
 
             double celsius, fahrenheit;
@@ -18,25 +18,29 @@ namespace TempConvert
                 case "F":
                     celsius = double.Parse(input);
                     fahrenheit = celsius * 1.8 + 32;
+                    Console.WriteLine($"{celsius} C = {fahrenheit} F");
                     break;
 
                 case "f":
                     celsius = double.Parse(input);
-                    fahrenheit = celsius * 1.8 + 32;
+                    fahrenheit = celsius * 1.8 + 32;                    
+                    Console.WriteLine($"{celsius} C = {fahrenheit} F");
                     break;
 
                 case "C":
                     fahrenheit = double.Parse(input);
-                    celsius = (fahrenheit - 32) / 1.8;
+                    celsius = (fahrenheit - 32) / 1.8;                    
+                    Console.WriteLine($"{celsius} C = {fahrenheit} F");
                     break;
 
                 case "c":
                     fahrenheit = double.Parse(input);
-                    celsius = (fahrenheit - 32) / 1.8;
+                    celsius = (fahrenheit - 32) / 1.8;                   
+                    Console.WriteLine($"{celsius} C = {fahrenheit} F");
                     break;
 
                 default:
-                    Console.WriteLine("");
+                    Console.WriteLine("Invalid unit");
                     break;
             }
         }
