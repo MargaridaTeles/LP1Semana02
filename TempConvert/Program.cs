f2fc972df79d7365ecc5370b3120785df30935ef
MargaridaTeles <telesmaegarida@gmail.com>
2026-02-26 16:41:12 +0000

    Fix formatting

--- TempConvert/Program.cs
+++ TempConvert/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace TempConvert
 {
@@ -6,37 +7,40 @@ namespace TempConvert
     {
         private static void Main(string[] args)
         {
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
             Console.Write("Temperature: ");
             string input = Console.ReadLine();
             Console.Write("Unit (C/F): ");
             string unidade = Console.ReadLine();
 
-            double celsius, fahrenheit;
+            double fahrenheit, celsius;
 
             switch (unidade)
             {
                 case "F":
                     celsius = double.Parse(input);
                     fahrenheit = celsius * 1.8 + 32;
-                    Console.WriteLine($"{celsius} C = {fahrenheit} F");
+                    Console.WriteLine($"{fahrenheit:f2} F = {celsius:f2} C");
                     break;
 
                 case "f":
                     celsius = double.Parse(input);
-                    fahrenheit = celsius * 1.8 + 32;                    
-                    Console.WriteLine($"{celsius} C = {fahrenheit} F");
+                    fahrenheit = celsius * 1.8 + 32;
+                    Console.WriteLine($"{fahrenheit:f2} F = {celsius:f2} C");
                     break;
 
                 case "C":
                     fahrenheit = double.Parse(input);
                     celsius = (fahrenheit - 32) / 1.8;                    
-                    Console.WriteLine($"{celsius} C = {fahrenheit} F");
+                    Console.WriteLine($"{celsius:f2} C = {fahrenheit:f2} F");
                     break;
 
                 case "c":
                     fahrenheit = double.Parse(input);
                     celsius = (fahrenheit - 32) / 1.8;                   
-                    Console.WriteLine($"{celsius} C = {fahrenheit} F");
+                    Console.WriteLine($"{celsius:f2} C = {fahrenheit:f2} F");
+                    //Math.Abs()
                     break;
 
                 default:
