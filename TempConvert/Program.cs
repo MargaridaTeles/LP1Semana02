c42d8ee3dd6717e9ea35e50bd895dfc0d67abda1
MargaridaTeles <telesmaegarida@gmail.com>
2026-02-26 16:16:59 +0000

    Add operations for Celsius and Fahrenheit

--- TempConvert/Program.cs
+++ TempConvert/Program.cs
@@ -8,6 +8,37 @@ namespace TempConvert
         {
             Console.Write("Insira um temperatura: ");
             string input = Console.ReadLine();
+            Console.Write("Insira a unidade de origem (C) Celsious ou (F) Fahrenheit: ");
+            string unidade = Console.ReadLine();
+
+            double celsius, fahrenheit;
+
+            switch (unidade)
+            {
+                case "F":
+                    celsius = double.Parse(input);
+                    fahrenheit = celsius * 1.8 + 32;
+                    break;
+
+                case "f":
+                    celsius = double.Parse(input);
+                    fahrenheit = celsius * 1.8 + 32;
+                    break;
+
+                case "C":
+                    fahrenheit = double.Parse(input);
+                    celsius = (fahrenheit - 32) / 1.8;
+                    break;
+
+                case "c":
+                    fahrenheit = double.Parse(input);
+                    celsius = (fahrenheit - 32) / 1.8;
+                    break;
+
+                default:
+                    Console.WriteLine("");
+                    break;
+            }
         }
     }
 }
